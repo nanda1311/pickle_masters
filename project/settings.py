2002d54c12a5b05bbdfacb39f94a902e562639dd
Adil Shabab <adilshabab@Adils-Mac-mini.local>
2026-03-10 15:53:36 +0530

    settings.py changed

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_zpdyd)kv)vrw%!j&kmsqz6fg5%@rhzsrmjalisw&y)$z8ku#%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.116', 'localhost', '127.0.0.1', '192.168.0.106']
+ALLOWED_HOSTS = ['192.168.0.116', 'localhost', '127.0.0.1', '192.168.0.106', '157.245.105.191']
 
 
 # Application definition
@@ -79,12 +79,26 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'picklemasterdb',
+        'USER': 'picklemasterdbuser',
+        'PASSWORD': 'Pickle@Master#Db',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
